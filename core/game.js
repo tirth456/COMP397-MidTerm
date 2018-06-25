2149960107e2bea285239c6e2dfc5f7f008ce0c2
Tirthraj <tirthraj456@gmail.com>
2018-06-25 11:06:33 -0400

    final commit

--- core/game.js
+++ core/game.js
@@ -31,15 +31,15 @@ function init() {
 		createjs.Ticker.setFPS(60);
 			createjs.Ticker.on("tick", update, this);
 		
-			var Image_Start = queue.getResult("start");
+		var Image_Start = queue.getResult("start");
 		var button_Start = new createjs.Bitmap(Image_Start);
-		button_Start.addEventListener("click", showNewRoll);
+		button_Start.addEventListener("click", NewRoll);
 		button_Start.x = 320;
 		button_Start.y = 430;
 		stage.addChild(button_Start);
 	}
 	
-		function showNewRoll() {
+		function NewRoll() {
 	
 		stage.removeChild(dice1);
 		stage.removeChild(dice2);
@@ -52,21 +52,21 @@ function init() {
 		var img1 = queue.getResult("dice" + txt1);
 		var img2 = queue.getResult("dice" + txt2);
 		
-			dice1 = new createjs.Bitmap(img1);
+		dice1 = new createjs.Bitmap(img1);
 		dice2 = new createjs.Bitmap(img2);
 		dice1.x = 640 - (dice1.getBounds().width * 2) - 50;
 		dice2.x = 640 - (dice1.getBounds().width) - 30;
 		dice1.y = 100;
 		dice2.y = 100;
 		 
-			txt_dice1 = new createjs.Text(txt1, "20px console", "#000");
+		txt_dice1 = new createjs.Text(txt1, "20px console", "#000");
 		txt_dice2 = new createjs.Text(txt2, "20px console", "#000");
 		txt_dice1.x = dice1.x + (dice1.getBounds().width * 0.5);
 		txt_dice2.x = dice2.x + (dice2.getBounds().width * 0.5);
 		txt_dice1.y = 100 + dice1.getBounds().height + 20;
 		txt_dice2.y = 100 + dice2.getBounds().height + 20;
 		
-			stage.addChild(dice1);
+		stage.addChild(dice1);
 		stage.addChild(dice2);
 		stage.addChild(txt_dice1);
 		stage.addChild(txt_dice2);
